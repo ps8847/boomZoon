9095d2ce433ad860de7c45c90ac192668b309840
Prince Sharma <ps789650@gmail.com>
2024-08-18 19:11:21 +0530

    updated time format

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,9 +2,9 @@ import MeetingTypeList from '@/components/MeetingTypeList';
 
 const Home = () => {
   const now = new Date();
-
-  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
+  const userLocale = navigator.language || 'en-US'; 
+  const time = now.toLocaleTimeString(userLocale, { hour: '2-digit', minute: '2-digit' });
+  const date = (new Intl.DateTimeFormat(userLocale, { dateStyle: 'full' })).format(now);
 
   return (
     <section className="flex size-full flex-col gap-5 text-white">
